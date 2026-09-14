ce4e07ee8be9f2cf6129a10276e96054b56e70ff
Daniels Nagornuks <nagornuk2004@gmail.com>
2026-09-14 11:59:20 +0100

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -56,3 +56,14 @@ function findFriends(friends: Friend[], criterion: (friend: Friend) => boolean):
 
 console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
 console.log(findFriends(friends, (friend) => friend.age < 35));
+
+function addInterest(friend: Friend, interest: string): string[] {
+  if (friend.interests === undefined) {
+    friend.interests = [interest];
+  } else {
+    friend.interests.push(interest);
+  }
+  return friend.interests;
+}
+
+console.log(addInterest(friends[0], 'Politics'))
